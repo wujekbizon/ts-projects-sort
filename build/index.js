62417165843aac32a8d63e18adcf44351b62233c
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-24 15:27:05 +0200

    test sort string algorithm

--- build/index.js
+++ build/index.js
@@ -2,9 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([
     27, 10, 0, -3, -67, 45, 134, 6, 0.5, 58,
 ]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+const charactersCollection = new CharactersCollection_1.Characterscollection('Grzegorz ma kota ');
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { Characterscollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([
   27, 10, 0, -3, -67, 45, 134, 6, 0.5, 58,
 ]);
-const sorter = new Sorter(numbersCollection);
+const charactersCollection = new Characterscollection('Grzegorz ma kota ');
+
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
