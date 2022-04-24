67aca77c2d7d7b4a37e744f6ad9057c21e21874f
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-24 12:16:47 +0200

    add sorting implementation logic

--- build/index.js
+++ build/index.js
@@ -3,5 +3,22 @@ class Sorter {
     constructor(collection) {
         this.collection = collection;
     }
+    // create sort method
+    sort() {
+        const { length } = this.collection;
+        // Sorting implementation
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    // if is greater then swap two elements
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
 }
-const sorter = new Sorter([-3, 0, 9, 27]);
+const sorter = new Sorter([27, 10, 0, -3, -67, 45, 134, 6, 0.5]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 class Sorter {
-  collection: number[];
+  constructor(public collection: number[]) {}
 
-  constructor(collection: number[]) {
-    this.collection = collection;
+  // create sort method
+  sort(): void {
+    const { length } = this.collection;
+
+    // Sorting implementation
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          // if is greater then swap two elements
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
   }
 }
 
-const sorter = new Sorter([-3, 0, 9, 27]);
+const sorter = new Sorter([27, 10, 0, -3, -67, 45, 134, 6, 0.5]);
+sorter.sort();
+console.log(sorter.collection);
